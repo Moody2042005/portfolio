45183bb006f61bb22c58cc98ec2be3603ab6b757
Mohamed tarek fayez <128873786+Moody2042005@users.noreply.github.com>
2026-05-02 01:21:45 +0300

    Update imports for App and CSS files

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import App from './App_2.tsx';
-import './index_2.css';
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
